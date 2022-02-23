0d9d35b1a05c426724925e7c154b6a831ced3aa4
Kris Gano <kris@pyxld.com>
2022-02-22 20:16:43 -0600

    Add Footer component + flexbox to Emote page module

--- src/components/modules/Emote/EmoteSection/EmoteSection.js
+++ src/components/modules/Emote/EmoteSection/EmoteSection.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import ContentSection from "../../../common/ContentSection/ContentSection";
 import InnerSection from "../../../common/InnerSection/InnerSection";
+import Footer from "../../../common/Footer/Footer";
 import Dropdown from "../../../common/Dropdown/Dropdown";
 import initializerMetadata from "../../../../utils/initializers-metadata-lookup.json";
 
@@ -44,22 +45,23 @@ export default function Emote(props) {
 
   return (
     <div className="Emote">
-        <ContentSection width="75%">
-        <Dropdown 
-          label="Emote:"
-          options={emotes}
-          value={displayEmote.displayEmote}
-          onChange={handleSelect}></Dropdown>
-        <InnerSection>
-        <div style={{border:"1px solid black", padding:"1rem", width:"75%", maxWidth:"500px", margin: "0 auto"}}>
-          <div>
-            <img src={initializerMetadata[tokenId][0].imageData} align="top" width="75%" alt={caption}/>
-            <img src={displayEmote.displayEmote + '.png'} align="top" width="25%"/>
-          </div>
-          <div style={{fontStyle: "italic",fontSize: "2rem", paddingTop: "1rem"}}>Initializer {initializer.initializer} {caption}</div>
-          </div>
-        </InnerSection>
+        <ContentSection width="75%" style={{minHeight:"80vh", display:"flex", flexDirection:"column", justifyContent:"center", alignItems:"center"}}>
+          <Dropdown 
+            label="Emote:"
+            options={emotes}
+            value={displayEmote.displayEmote}
+            onChange={handleSelect}></Dropdown>
+          <InnerSection>
+          <div style={{border:"1px solid black", padding:"1rem", width:"75%", maxWidth:"500px", margin: "0 auto"}}>
+            <div>
+              <img src={initializerMetadata[tokenId][0].imageData} align="top" width="75%" alt={caption}/>
+              <img src={displayEmote.displayEmote + '.png'} align="top" width="25%"/>
+            </div>
+            <div style={{fontStyle: "italic",fontSize: "2rem", paddingTop: "1rem"}}>Initializer {initializer.initializer} {caption}</div>
+            </div>
+          </InnerSection>
         </ContentSection>
+        <Footer />
     </div>
   );
 }
