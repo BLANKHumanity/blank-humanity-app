db70bcfc4f5b3b0b6ca70bc524e45ff9a4daf3cf
Kris Gano <kris@pyxld.com>
2022-02-22 20:15:49 -0600

    Remove absolute positioning from Dropdown component

--- src/components/common/Dropdown/Dropdown.js
+++ src/components/common/Dropdown/Dropdown.js
@@ -4,7 +4,7 @@ export default function Dropdown ({ label, value, options, onChange }) {
   return (
     <label>
       {label}
-      <select value={value} onChange={onChange} style={{position: "absolute", top: "5px", height: "30px"}}>
+      <select value={value} onChange={onChange} style={{height: "30px"}}>
         {options.map((option) => (
           <option key={option.label} value={option.value}>{option.label}</option>
         ))}
