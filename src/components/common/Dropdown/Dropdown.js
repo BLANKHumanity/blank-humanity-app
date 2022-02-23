3120bfd0e2935dfdaf37b497d1dedbc3b2ed5133
Peter Newell <petersgrandadventure@gmail.com>
2022-02-22 19:46:00 -0800

    fix alignment on larger breakpoints

--- src/components/common/Dropdown/Dropdown.js
+++ src/components/common/Dropdown/Dropdown.js
@@ -2,13 +2,15 @@ import React from "react";
 
 export default function Dropdown ({ label, value, options, onChange }) {
   return (
-    <label>
-      {label}
-      <select value={value} onChange={onChange} style={{height: "30px"}}>
-        {options.map((option) => (
-          <option key={option.label} value={option.value}>{option.label}</option>
-        ))}
-      </select>
-    </label>
+    <div>
+      <label>
+        {label}
+        <select value={value} onChange={onChange} style={{height: "30px", paddingTop: "5px"}}>
+          {options.map((option) => (
+            <option key={option.label} value={option.value}>{option.label}</option>
+          ))}
+        </select>
+      </label>
+    </div>
   );
 };
