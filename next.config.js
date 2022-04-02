fc47588b9cac6ef562fc23239f53df70f0fac506
Peter Newell <petersgrandadventure@gmail.com>
2022-04-01 21:55:35 -0700

    reroute single emote (/Emote/tokenId/emote) to (api/emote/:tokenId/:emote)

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  poweredByHeader: false,
-}
+    async redirects() {
+      return [
+        {
+          source: '/Emote/:tokenId/:emote',
+          destination: '/api/emote/:tokenId/:emote', // Matched parameters can be used in the destination
+          permanent: true,
+        },
+      ]
+    },
+  }
