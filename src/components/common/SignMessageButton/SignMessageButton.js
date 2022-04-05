ce8711715a37dda6d956782b576ba5ebdf55917e
Peter Newell <petersgrandadventure@gmail.com>
2022-04-05 12:42:23 -0700

    better message handling

--- src/components/common/SignMessageButton/SignMessageButton.js
+++ src/components/common/SignMessageButton/SignMessageButton.js
@@ -10,34 +10,47 @@ export default function SignMessageButton({
     const [display, setDisplay] = React.useState("none");
     const [disabled, setDisabled] = React.useState(false);
     const [buttonText, setButtonText] = React.useState("SIGN MESSAGE");
+    const [responseMessage, setResponseMessage] = React.useState("");
   
     React.useEffect(async () => {
       const cssDisplay = accounts.length == 0 ? "none" : "inline";
       setDisplay(cssDisplay);
     }, [accounts]);
     return (
-      <button
-        style={{ display: display }}
-        className="ButtonBlack"
-        disabled={disabled}
-        onClick={async () => {
-            var signature = await web3.eth.personal.sign(message, accounts[0], function(error, signature) {
-                fetch('/api/verify', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify({
-                        salt: salt, 
-                        signature: signature, 
-                        address:accounts[0]
-                    }),
-                }).then(res => res.status == 200 ? setDisabled(true) : setButtonText("TRY AGAIN"))
-            })
-        }}
-      >
-        {disabled?"SUCCESS: You can close this window":buttonText}
-      </button>
+      <div>
+        <p className="message">{responseMessage}</p>
+        <button
+          style={{ display: display }}
+          className="ButtonBlack"
+          disabled={disabled}
+          onClick={async () => {
+              var signature = await web3.eth.personal.sign(message, accounts[0], function(error, signature) {
+                  fetch('/api/verify', {
+                      method: 'POST',
+                      headers: {
+                          'Content-Type': 'application/json',
+                      },
+                      body: JSON.stringify({
+                          salt: salt, 
+                          signature: signature, 
+                          address:accounts[0]
+                      }),
+                  }).then(res => {
+                    if(res.status == 200) {
+                      setResponseMessage("SUCCESS: You can close this window");
+                      setButtonText("ALL DONE");
+                      setDisabled(true);
+                    } else {
+                      setButtonText("TRY AGAIN");
+                      setResponseMessage("There was an error processing the signature, please try again. If this problem persists, try starting over from discord.")
+                    } 
+                  })
+              })
+          }}
+        >
+          {buttonText}
+        </button>
+      </div>
     );
   }
   
