3a96363b3748910a01dee1d0bf63a3e5fdad608d
Peter Newell <petersgrandadventure@gmail.com>
2022-04-05 11:37:55 -0700

    make first letter case insensitive

--- next.config.js
+++ next.config.js
@@ -5,7 +5,16 @@ module.exports = {
           source: '/Emote/:tokenId/:emote',
           destination: '/api/emote/:tokenId/:emote', // Matched parameters can be used in the destination
           permanent: true,
-        },
+        }
       ]
     },
+    async rewrites() {
+      return [{
+        source: '/(V|v)erify',
+        destination: '/Verify', // Matched parameters can be used in the destination
+      },{
+        source: '/(E|e)mote/:tokenId',
+        destination: '/Emote/:tokenId', // Matched parameters can be used in the destination
+      }]
+    }
   }
